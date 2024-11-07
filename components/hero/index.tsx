16288c45a543c6215a394258b26bae51a25e9de1
mtvanli <mehmet.vanli@gmail.com>
2024-11-07 22:18:01 +1100

    New case studies added to the home page

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -12,9 +12,10 @@ import {
   SlideshowSlide,
 } from '~/components/ui/slideshow';
 
-import SlideshowBG from './slideshow-bg-01.jpg';
-import SlideshowBG3 from './slideshow-bg-02.jpg';
+import SlideshowBG from './slideshow-bg-05.jpg';
+import SlideshowBG3 from './slideshow-bg-04.jpg';
 import SlideshowBG4 from './slideshow-bg-03.jpg';
+
 
 const SlideshowBlurDataURL =
   'data:image/jpeg;base64,/9j/4QC8RXhpZgAASUkqAAgAAAAGABIBAwABAAAAAQAAABoBBQABAAAAVgAAABsBBQABAAAAXgAAACgBAwABAAAAAgAAABMCAwABAAAAAQAAAGmHBAABAAAAZgAAAAAAAABIAAAAAQAAAEgAAAABAAAABgAAkAcABAAAADAyMTABkQcABAAAAAECAwAAoAcABAAAADAxMDABoAMAAQAAAP//AAACoAQAAQAAAAoAAAADoAQAAQAAAAcAAAAAAAAA/9sAQwADAgIDAgIDAwMDBAMDBAUIBQUEBAUKBwcGCAwKDAwLCgsLDQ4SEA0OEQ4LCxAWEBETFBUVFQwPFxgWFBgSFBUU/9sAQwEDBAQFBAUJBQUJFA0LDRQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQU/8AAEQgABwAKAwERAAIRAQMRAf/EABUAAQEAAAAAAAAAAAAAAAAAAAMJ/8QAIBAAAQQBBAMAAAAAAAAAAAAAAQIDBAURABIhMQYjgf/EABYBAQEBAAAAAAAAAAAAAAAAAAEAAv/EABkRAAIDAQAAAAAAAAAAAAAAAAARAQIhQf/aAAwDAQACEQMRAD8AoZ5EzayKWW3Syo0GyKPTJlsF9ts9klsKTu46GQOfms2awJfAKywmt1sRNgqK7PS0gSHI4WltTmBuKQckJJzgE9aYa0tP/9k=';
@@ -25,9 +26,9 @@ export const Hero = () => (
       <SlideshowSlide>
         <div className="relative ">
           <Image
-            alt="Just Sunnies slide image"
+            alt="Designerie slide image"
             blurDataURL={SlideshowBlurDataURL}
-            className="absolute -z-10 object-cover object-right-top"
+            className="absolute -z-10 object-cover object-right"
             fill
             placeholder="blur"
             priority
@@ -37,11 +38,11 @@ export const Hero = () => (
           />
           <div className="absolute inset-0 bg-black opacity-30 -z-10" />
           <div className="flex flex-col h-[548px] gap-4 px-12 pb-48 pt-56 lg:pt-36">
-            <h2 className="text-4xl text-neutral-50 font-black lg:text-6xl mt-10 ">Just Sunnies</h2>
-            <p className="text-md lg:text-lg max-w-xl text-neutral-50">Uses Composable Commerce to Accelerate Sales
+            <h2 className="text-4xl text-neutral-50 font-black lg:text-6xl mt-10 ">Designerie</h2>
+            <p className="text-md lg:text-lg max-w-xl text-neutral-50">Builds a Luxury Ecommerce Site with BigCommerce
             </p>
             <Button asChild className="w-fit mt-4">
-              <a href="https://www.bigcommerce.com/case-study/just-sunnies/" target="_blank" rel="noopener noreferrer">Case Study</a>
+              <a href="https://www.bigcommerce.com/case-study/designerie/" target="_blank" rel="noopener noreferrer">Case Study</a>
             </Button>
           </div>
         </div>
@@ -49,9 +50,9 @@ export const Hero = () => (
       <SlideshowSlide>
       <div className="relative ">
       <Image
-            alt="White Stuff slide image"
+            alt="Grenson slide image"
             blurDataURL={SlideshowBlurDataURL}
-            className="absolute -z-10 object-cover  object-right-top"
+            className="absolute -z-10 object-cover  object-right"
             fill
             placeholder="blur"
             priority
@@ -60,12 +61,12 @@ export const Hero = () => (
           />
           <div className="absolute inset-0 bg-black opacity-30 -z-10" />
           <div className="flex flex-col h-[548px] gap-4 px-12 pb-48 pt-56 lg:pt-36 ">
-          <h2 className="text-4xl text-neutral-50 font-black lg:text-6xl mt-10 ">White Stuff</h2>
+          <h2 className="text-4xl text-neutral-50 font-black lg:text-6xl mt-10 ">Grenson</h2>
           <p className="text-md lg:text-lg max-w-xl text-neutral-50">
-          Quickly Lifts Customer Experience with BigCommerce
+          Steps Up Its Ecommerce with BigCommerce
           </p>
           <Button asChild className="w-fit mt-4">
-            <a href="https://www.bigcommerce.com/case-study/white-stuff/" target="_blank" rel="noopener noreferrer">Case Study</a>
+            <a href="https://www.bigcommerce.com/case-study/grenson/" target="_blank" rel="noopener noreferrer">Case Study</a>
           </Button>
         </div>
         </div>
